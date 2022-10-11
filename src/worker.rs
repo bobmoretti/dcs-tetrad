870bb64432339a73839db0a4b539db71312c4929
Bobby Moretti <bobmoretti@gmail.com>
2022-10-11 04:43:14 -0700

    Add separate (much smaller) csv for just logging frame rate

--- src/worker.rs
+++ src/worker.rs
@@ -35,10 +35,9 @@ fn format_now() -> String {
     date.format("%Y-%m-%d %H-%M-%S").to_string()
 }
 
-fn setup_object_log(
-    mission_name: &String,
-    dir_name: &Path,
-) -> csv::Writer<ZstdEncoder<'static, File>> {
+fn create_csv_file(mission_name: &str, dir_name: &Path) -> csv::Writer<ZstdEncoder<'static, File>> {
+    std::fs::create_dir_all(&dir_name).unwrap();
+
     let fname = dir_name.join(format!("{} - {}.csv.zstd", mission_name, format_now()));
     log::debug!("Trying to open csv file: {:?}", fname);
 
@@ -73,20 +72,38 @@ fn finish<W: std::io::Write>(obj: &mut Option<csv::Writer<W>>) {
     }
 }
 
+fn log_frame(writer: &mut csv::Writer<zstd::Encoder<'_, File>>, t1: f64, t0: f64, n: i32) {
+    if t0 != t1 {
+        writer.write_field((n - 1).to_string()).unwrap();
+        writer.write_field((t1 - t0).to_string()).unwrap();
+        writer.write_record(None::<&[u8]>).unwrap();
+    }
+}
+
 pub fn entry(config: Config, mission_name: String, rx: Receiver<Message>) {
+    let mut prev_frame_time: f64;
     let mut most_recent_time: f64 = 0.0;
     let mut frame_count: i32 = 0;
-    let dir_name = Path::new(config.write_dir.as_str())
+    let log_dir = Path::new(config.write_dir.as_str())
         .join("Logs")
         .join("Tetrad");
-    std::fs::create_dir_all(&dir_name).unwrap();
     log::debug!("Starting with config {:?}", config);
 
-    let mut csv_writer = if config.enable_object_log {
-        Some(setup_object_log(&mission_name, &dir_name))
+    let mut object_writer = if config.enable_object_log {
+        let writer = create_csv_file(&mission_name, &log_dir.join("objects"));
+        Some(writer)
     } else {
         None
     };
+
+    let mut frame_writer = if config.enable_framerate_log {
+        let writer = create_csv_file(&mission_name, &log_dir.join("frames"));
+        Some(writer)
+    } else {
+        None
+    };
+    // fuck the static analyzer... ugh
+    // let logs = [&object_writer, &frame_writer];
 
     loop {
         log::trace!("Waiting for message");
@@ -94,25 +111,23 @@ pub fn entry(config: Config, mission_name: String, rx: Receiver<Message>) {
         match msg {
             Message::NewFrame(t) => {
                 frame_count += 1;
-                most_recent_time = t;
                 log::trace!("New frame message, n = {}, t = {}", frame_count, t);
+                prev_frame_time = most_recent_time;
+                most_recent_time = t;
+                if let Some(ref mut w) = frame_writer {
+                    log_frame(w, t, prev_frame_time, frame_count);
+                }
             }
             Message::BallisticsStateUpdate(objects) => {
                 log::trace!("Logging Ballistics message with {} elements", objects.len());
-                match csv_writer {
-                    Some(ref mut writer) => {
-                        log_dcs_objects(frame_count, most_recent_time, writer, objects)
-                    }
-                    None => (),
+                if let Some(ref mut writer) = object_writer {
+                    log_dcs_objects(frame_count, most_recent_time, writer, objects);
                 }
             }
             Message::UnitStateUpdate(objects) => {
                 log::trace!("Logging Units message with {} elements", objects.len());
-                match csv_writer {
-                    Some(ref mut writer) => {
-                        log_dcs_objects(frame_count, most_recent_time, writer, objects)
-                    }
-                    None => (),
+                if let Some(ref mut writer) = object_writer {
+                    log_dcs_objects(frame_count, most_recent_time, writer, objects)
                 }
             }
             Message::Stop => {
@@ -121,6 +136,8 @@ pub fn entry(config: Config, mission_name: String, rx: Receiver<Message>) {
             }
         }
     }
-    log::debug!("finishing csv file!");
-    finish(&mut csv_writer);
+    log::debug!("finishing csv files!");
+
+    finish(&mut object_writer);
+    finish(&mut frame_writer);
 }
